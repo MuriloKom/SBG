cb5f2c1c6cb6efc9ac527a435d7af6b3fb7cfc95
MuriloKom <murilo.nt94@gmail.com>
2016-12-16 13:43:39 -0200

    Deprecated. Using authentication-confirmation.php

--- SBG/php/auth.php
+++
@@ -1,9 +0,0 @@
- <?php
-//Inicia a sessão
-session_start();
-//Verifica se a variável SESS_MEMBER_ID está setada ou não
-if (!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
-    header("location: index.php");
-    exit();
-}
-?> 
